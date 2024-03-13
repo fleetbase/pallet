47ac23a2448577cb4991345914a731eba730b6cf
Turtuvshin <toroo.byamba@gmail.com>
2024-03-13 15:21:16 +0800

    Remove unused context component utilities and imported from ember-core
    `applyContextComponentArguments`, `contextComponentCallback` functions moved to `ember-core` for shared usage.

--- addon/components/sales-order-panel.js
+++ addon/components/sales-order-panel.js
@@ -4,8 +4,8 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { isArray } from '@ember/array';
 import SalesOrderPanelDetailsComponent from './sales-order-panel/details';
-import contextComponentCallback from '../utils/context-component-callback';
-import applyContextComponentArguments from '../utils/apply-context-component-arguments';
+import contextComponentCallback from '@fleetbase/ember-core/utils/context-component-callback';
+import applyContextComponentArguments from '@fleetbase/ember-core/utils/apply-context-component-arguments';
 
 export default class SalesOrderPanelComponent extends Component {
     /**
